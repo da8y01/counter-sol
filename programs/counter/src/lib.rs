eb9d22fd59ce7d00a62c6b45fb9c77b3056f920c
Developer Localhost <developer@local.host>
2024-06-22 13:41:25 -0500

    wip: Create and Delete.

--- programs/counter/src/lib.rs
+++ programs/counter/src/lib.rs
@@ -6,10 +6,50 @@ declare_id!("GYb4aembe4KUgHV27LNadiNnNZqDVLEdBmAqXJpmbUQT");
 pub mod counter {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_counter(_ctx: Context<Crear>, primer_numero: u64) -> Result<()> {
+        _ctx.accounts.contador.numero = primer_numero;
+        _ctx.accounts.contador.autoridad = _ctx.accounts.autoridad.key();
+        msg!("creando un contador con numero inicial {} ", primer_numero);
+        Ok(())
+    }
+
+    pub fn delete_counter(_ctx: Context<Borrar>) -> Result<()> {
+        msg!("Contador eliminado");
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(count: u64)]
+pub struct Crear<'info> {
+    // 8 bytes para discriminador  + (lo que ocupe tu estructura)
+    #[account(init, payer = autoridad, space = 8 + 8 + 32)]
+    pub contador: Account<'info, Contador>,
+    #[account(mut)]
+    pub autoridad: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Borrar<'info> {
+    #[account(mut)]
+    pub autoridad: Signer<'info>,
+    #[account(
+        mut,
+        constraint = contador.autoridad == contador.key(),
+        close = autoridad
+    )]
+    pub contador: Account<'info, Contador>,
+}
+
+#[account]
+pub struct Contador {
+    numero: u64,       // 8 bytes
+    autoridad: Pubkey, // 32 bytes
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("You are not authorized.")]
+    NotAuthorized,
+}
--- tests/counter.ts
+++ tests/counter.ts
@@ -1,16 +1,30 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Counter } from "../target/types/counter";
+import { BN } from "bn.js";
 
 describe("counter", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Counter as Program<Counter>;
 
-  it("Is initialized!", async () => {
+  it("Create a counter", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const count = new BN(0);
+    const counterKp = new anchor.web3.Keypair();
+    const tx = await program.methods.createCounter(count).accounts({
+      counter: counterKp.publicKey,
+      authority: provider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    })
+    .signers([counterKp])
+    .rpc();
     console.log("Your transaction signature", tx);
+
+    const counter = await program.account.counter.fetch(counterKp.publicKey)
+    console.log("counter count is: ")
+    assert(count.eq(counter.count))
   });
 });
